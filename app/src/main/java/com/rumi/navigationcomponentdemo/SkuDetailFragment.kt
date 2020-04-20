5e8b4d078e59557afa5f663914760bd241a04531
rumi <rumi@Rumis-MacBook-Pro.local>
2020-04-20 17:37:53 +0545

    image capture

--- app/src/main/java/com/rumi/navigationcomponentdemo/SkuDetailFragment.kt
+++ app/src/main/java/com/rumi/navigationcomponentdemo/SkuDetailFragment.kt
@@ -1,12 +1,21 @@
 package com.rumi.navigationcomponentdemo
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.evolve.rosiautils.PictureManager
+import com.evolve.rosiautils.TYPE_ERROR
+import com.evolve.rosiautils.TYPE_SUCCESS
+import com.evolve.rosiautils.loadImage
+import com.evolve.rosiautils.showToast
+import kotlinx.android.synthetic.main.fragment_sku_detail.*
 
 class SkuDetailFragment : Fragment() {
+
+    private lateinit var pictureManager: PictureManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,5 +23,38 @@ class SkuDetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sku_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        pictureManager = PictureManager(this)
+        btn_take_pic.setOnClickListener {
+            context?.let {
+                if (pictureManager.hasPermission(it)) {
+                    openCamera()
+                }
+            }
+        }
+    }
+
+    private fun openCamera() {
+        pictureManager.startCameraIntent(requireContext()) { imgPath ->
+            loadImage(img_sku, imgPath) {
+                if (!it) {
+                    showToast("something went wrong", TYPE_ERROR)
+                } else
+                    showToast("success", TYPE_SUCCESS)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (pictureManager.onRequestPermissionsResult(requestCode, permissions, grantResults)) openCamera()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        pictureManager.onActivityResult(requestCode, resultCode, data)
     }
 }
